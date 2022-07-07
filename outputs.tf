ccf76de566c3c0dbb78316e2746cd36fbd3d5cb8
Javier Bravo <jrbv@gft.com>
2022-07-07 15:49:50 +0200

    added new jenkisfile function

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.terraformAWS.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.terraformAWS.public_ip
-}
